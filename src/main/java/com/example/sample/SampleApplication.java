123f3b4a8c16decb312ec2be9b84658d3100e16d
中村順 <n.slenderness@gmail.com>
2022-12-27 16:31:42 +0900

    add datasource and change to log42

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -1,13 +1,18 @@
 package com.example.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+		logger.info("started sample application");
 	}
 
 }
